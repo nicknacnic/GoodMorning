45021a81558917a1bb53648b242fe6282825994e
Layer8 <NWillAU900@gmail.com>
2023-12-29 20:57:01 -0700

    Update cron.sh
    2 hour delay maximum

--- cron.sh
+++ cron.sh
@@ -2,7 +2,7 @@
 
 # Define the time window (in seconds) within which you want to randomize
 MIN_DELAY=0     # Minimum delay in seconds (0 seconds)
-MAX_DELAY=7200  # Maximum delay in seconds (1 hour)
+MAX_DELAY=7200  # Maximum delay in seconds (2 hours)
 
 # Generate a random delay within the specified time window
 RANDOM_DELAY=$((RANDOM % ($MAX_DELAY - $MIN_DELAY + 1) + $MIN_DELAY))
